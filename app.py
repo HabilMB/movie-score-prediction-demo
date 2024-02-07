63c2f020c8ff6c92011b48f9a864733d801b5c7c
habil <habilmediob@gmail.com>
2024-02-07 19:49:40 +0700

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-
-#import cv2
-import json
-#from tensorflow.keras.application.imagenet_utils import decode_predictions
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import tensorflow as tf
-
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('saved_model')
+model = tf.keras.models.load_model('saved_model_pycharm')
 
 @app.route('/')
 def home():
@@ -26,14 +21,13 @@ def predict():
 @app.route('/predict', methods=['POST'])
 def prediction():
     user_input = request.form.to_dict()
-    user_input['runtime'] = int(user_input['runtime'])
-    user_input['budget'] = int(user_input['budget'])
+    user_input['year'] = int(user_input['year'])
 
     converted = {name: tf.convert_to_tensor([value]) for name, value in user_input.items()}
     predict = model.predict(converted)
     final_pred = predict[0][0]
     verdict = ""
-
+          
     if final_pred >= 9:
         verdict = "Amazing!"
     elif final_pred >= 8:
@@ -50,7 +44,7 @@ def prediction():
         verdict = "Terrible"
 
     return render_template("prediction.html", prediction ="{:.1f}".format(final_pred), verdict = verdict)
+    
 
 if __name__ == '__main__':
-    #app.run(port=3000, debug=True)
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(port=3000, debug=True)
